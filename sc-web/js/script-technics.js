1fb3184e0b205fe777c256853cf237c4e5f95fb4
Evgeny <pobezhimovskiy@gmail.com>
2021-06-22 11:35:43 +0300

    To refresh

--- sc-web/js/script-technics.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  $('.options__item').on('click', function() {
-    $(this).closest('ul.std-ul').find('div.options__descr')
-    .eq($(this).index()).fadeToggle('fast');
-    });
-
-});
